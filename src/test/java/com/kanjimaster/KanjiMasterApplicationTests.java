4ebb63dd6069f94f278f6e4f72a3ad8c06bc43e0
Oleh Skrypnyk <oskrypnyk@griddynamics.com>
2023-10-08 19:20:36 +0200

    Init

---
+++ src/test/java/com/kanjimaster/KanjiMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanjimaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
